92e98cffdf30dc776e943638465e396c4fe93e84
Khalil Chahoua <Khalil Chahoua@DESKTOP-NUSUTV2>
2024-05-19 19:01:10 -0400

    commit Methode Hello World

--- src/git/Git.java
+++ src/git/Git.java
@@ -15,6 +15,8 @@ public class Git {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        System.out.println("Hello World!");
+    }   
+    
     
 }
